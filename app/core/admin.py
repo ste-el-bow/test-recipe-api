4455c4c8632067efad3f60e9ba1f5f8872e1e93e
utytlanyjoe <lewandowski.dariusz@windowslive.com>
2020-09-30 10:55:37 +0200

    configured postgresdb

--- app/core/admin.py
+++ app/core/admin.py
@@ -18,10 +18,11 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': ('email', 'password1', 'password2')
-    }),
-)
+        (None, {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
